192f9eb45d7610e7ab98efc893536ce931e2de25
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-06-22 21:27:47 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Parent from './Parent';
+import React, { useState } from 'react';
 
 function App() {
+  let [nummber,setnumber]=useState(45)
+
   return (
+    
     <div>
       Hello World
+
+      <Parent num={nummber}></Parent>
 
 
     </div>
