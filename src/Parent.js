78c1aa992fa7fa050186c2e29a703e8e354779e3
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-06-22 21:22:57 +0500

    Create Parent.js

---
+++ src/Parent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Child from './Child';
+
+function Parent() {
+  return (
+    <div>
+      Parent
+       
+      <Child></Child>
+
+    </div>
+  );
+}
+
+export default Parent;
