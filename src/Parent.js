bc48c3c7ad7e4c51e042fb838d4f0cff5f5a01e1
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-06-22 21:27:33 +0500

    Update Parent.js

--- src/Parent.js
+++ src/Parent.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Child from './Child';
 
-function Parent() {
+function Parent(props) {
   return (
+    
     <div>
       Parent
        
-      <Child></Child>
+      <Child num={props.num}></Child>
 
     </div>
   );
